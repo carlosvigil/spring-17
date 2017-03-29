0aadb88a34ab41fc3871fb3a9d7f6c00c1ba7f39
Carlos Vigil <carlos.vigil@icloud.com>
2017-03-29 19:34:02 -0400

    rename files properly, finish hotel + rainfall

--- oocpp/code/hotel_occupancy.cpp
+++ oocpp/code/hotel_occupancy.cpp
@@ -4,17 +4,24 @@ using namespace std;
 int main()
 {
     // variable declarations
-    int total_floors = 0,   // the number of floors in the hotel
-        rooms = 0,          //
-        total_rooms = 0,    //
-        occupied_rooms = 0, //
-        total_occupied = 0, //
-        total_unoccupied = 0;
-    double percent_occupied = 0.0; //
+    int total_floors = 0,          // the number of floors in the hotel
+        rooms = 0,                 // the number of rooms on a floor
+        total_rooms = 0,           // the total of rooms in the hotel
+        occupied_rooms = 0,        // the number of occupied rooms on a floor
+        total_occupied = 0,        // the total of occupied rooms in the hotel
+        total_unoccupied = 0;      // the total remainder of rooms unoccupied
+    double percent_occupied = 0.0; // a percentage of the hotel occupancy
 
     // ask for number of floors
     cout << "How many floors are in the hotel?\n";
     cin >> total_floors;
+    // input validation
+    while (!cin.good() || total_floors < 1)
+    {
+        cin.clear();
+        cout << "\nTry again.\nEnter the total number of floors in the hotel:\n";
+        cin >> total_floors;
+    }
 
     // loop through each floor
     for (int i = 0; i < total_floors; i++)
@@ -22,13 +29,27 @@ int main()
         // let user know what floor we're asking about
         cout << "\nFloor: " << i + 1 << endl;
         // ask for floor variable value
-        cout << "How many rooms are there? ";
+        cout << "How many rooms are there?\n";
         cin >> rooms;
+        // input validation
+        while (!cin.good() || rooms < 0)
+        {
+            cin.clear();
+            cout << "\nTry again.\nEnter the number of rooms on this floor:\n";
+            cin >> rooms;
+        }
         // add floor number to total_rooms
         total_rooms += rooms;
         // ask for number of occupied rooms
-        cout << "How many of those are occupied? ";
+        cout << "How many of those are occupied?\n";
         cin >> occupied_rooms;
+        // input validation
+        while (!cin.good() || occupied_rooms < 0)
+        {
+            cin.clear();
+            cout << "\nTry again.\nEnter the number of occupied rooms on the floor:\n";
+            cin >> occupied_rooms;
+        }
         // add occupied_rooms value to the total of total_occupied
         total_occupied += occupied_rooms;
     }
--- oocpp/code/practice.cpp
+++ oocpp/code/practice.cpp
@@ -4,42 +4,67 @@ using namespace std;
 int main()
 {
     // variable declarations
-    int years;               // the number of years to record
-    int rainfall;            // the monthly rainfall in inches
-    int total_rainfall = 0;  // the total rainfall
-    double average_rainfall; // the average rainfall per month
+    int total_floors = 0,          // the number of floors in the hotel
+        rooms = 0,                 // the number of rooms on a floor
+        total_rooms = 0,           // the total of rooms in the hotel
+        occupied_rooms = 0,        // the number of occupied rooms on a floor
+        total_occupied = 0,        // the total of occupied rooms in the hotel
+        total_unoccupied = 0;      // the total remainder of rooms unoccupied
+    double percent_occupied = 0.0; // a percentage of the hotel occupancy
 
-    // ask for number of years
-    cout << "\nEnter the number of years:\n";
-    cin >> years;
-
-    while (!cin.good() || years < 0)
+    // ask for number of floors
+    cout << "How many floors are in the hotel?\n";
+    cin >> total_floors;
+    // input validation
+    while (!cin.good() || total_floors < 1)
     {
         cin.clear();
-        cout << "Try again.\nEnter the number of years:\n";
-        cin >> years;
+        cout << "\nTry again.\nEnter the total number of floors in the hotel:\n";
+        cin >> total_floors;
     }
 
     // loop through each floor
-    for (int y = 0; y < years; y++)
+    for (int i = 0; i < total_floors; i++)
     {
-        for (int m = 0; m < 12; m++)
+        // let user know what floor we're asking about
+        cout << "\nFloor: " << i + 1 << endl;
+        // ask for floor variable value
+        cout << "How many rooms are there?\n";
+        cin >> rooms;
+        // input validation
+        while (!cin.good() || rooms < 0)
         {
-            cout << "What is the monthly rainfall in inches?\n";
-            cin >> rainfall;
-            while (!cin.good() || rainfall < 0)
-            {
-                cin.clear();
-                cout << "Try again.\nEnter the rainfall in inches:\n";
-                cin >> rainfall;
-            }
-            total_rainfall += rainfall;
+            cin.clear();
+            cout << "\nTry again.\nEnter the number of rooms on this floor:\n";
+            cin >> rooms;
         }
+        // add floor number to total_rooms
+        total_rooms += rooms;
+        // ask for number of occupied rooms
+        cout << "How many of those are occupied?\n";
+        cin >> occupied_rooms;
+        // input validation
+        while (!cin.good() || occupied_rooms < 0)
+        {
+            cin.clear();
+            cout << "\nTry again.\nEnter the number of occupied rooms on the floor:\n";
+            cin >> occupied_rooms;
+        }
+        // add occupied_rooms value to the total of total_occupied
+        total_occupied += occupied_rooms;
     }
 
-    // calculate average
-    average_rainfall = static_cast<double>(total_rainfall) / (12 * years);
-    cout << endl << total_rainfall << endl << average_rainfall << endl << years * 12 << endl;
+    // do calculations for percentage of occupied_rooms and total_unoccupied
+    percent_occupied = static_cast<double>(total_occupied) / total_rooms;
+    percent_occupied *= 100.00;
+    total_unoccupied = total_rooms - total_occupied;
 
-        return 0;
+    // display results
+    cout << "\nResult:\n"
+         << "Total number of rooms: " << total_rooms << endl
+         << "Total number of rooms occupied: " << total_occupied << endl
+         << "Total number of rooms unoccupied: " << total_unoccupied << endl
+         << "Percent occupied: " << percent_occupied << "%\n";
+
+    return 0;
 }
