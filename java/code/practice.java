b933f11b60c9d81de2aec9155781a53c64c21652
Carlos Vigil <carlos.vigil@icloud.com>
2017-04-04 14:01:39 -0400

    +1 Java program

--- java/code/practice.java
+++ java/code/practice.java
@@ -2,12 +2,41 @@
 	Carlos Vigil
 	Dr. C. Horvath
 	Java @ GCC
-	March 27th, 2017
-	Chapter 5: #1 showChar Method
+	April 4th, 2017
+	Chapter 5
+
+	#15. even/odd counter
+	You can use the following logic to determine whether a number is even or odd:
+
+	if ((number % 2) == 0) {
+	// The number is even. }
+	else {
+	// The number is odd. }
+
+	Write a program with a method named isEven that accepts an int argument.
+	The method should return true if the argument is even, or false otherwise.
+	The program’s main method should use a loop to generate 100 random
+	integers. It should use the isEven method to determine whether each random
+	number is even, or odd. When the loop is finished, the program should
+	display the number of even numbers that were generated, and the number of
+	odd numbers.
+
 */
 
 public class practice {
 	public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++) {
+			isEven(i);
+		}
+	}
 
+	/**
+	 */
+	public static boolean isEven(int num) {
+		if ((num % 2) == 0) {
+			System.out.println("The number is even");
+		} else {
+			System.out.println("The number is odd");
+		}
 	}
 }
