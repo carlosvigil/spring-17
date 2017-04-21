f93941ec6d6136f4bac041e884f12c4070588896
Carlos Vigil <carlos.vigil@icloud.com>
2017-04-20 22:21:01 -0400

    add java assignments

--- java/code/practice.java
+++ java/code/practice.java
@@ -1,118 +1,36 @@
-import java.util.Set;
-
-import com.sun.xml.internal.messaging.saaj.packaging.mime.util.QDecoderStream;
-
-/**
+/*
 	Carlos Vigil
 	Dr. C. Horvath
 	Java @ GCC
-	April 17th, 2017
-	Chapter 6 - Employee Class
+	April 19th, 2017
+	Chapter 6 - Personal Information Class
 */
 
-public class Employee {
-    private int idNumber;
-	private String name;
-	private String department;
-	private String position;
+import java.util.Scanner;
 
-	/**
-	 * Full Constructor accepts 4 parameters and sets each for the instance
-	 * @param name a String object that stores the employees name
-	 * @param idNumber an integer that stores the employees ID number
-	 * @param department a String object that stores the employees department
-	 * @param position a String object that stores the employees job title
-	*/
-	public Employee (String n, int id, String dep, String pos) {
-		name = n;
-		idNumber = id;
-		department = dep;
-		position = pos;
-	}
+public class Practice {
 
-	/**
-	 * Half Constructor accepts two parameters and sets remaining two as ""
-	 * @param name a String object that stores the employees name
-	 * @param idNumber an integer that stores the employees ID number
-	*/
-	public Employee (String n, int id) {
-		name = n;
-		idNumber = id;
-		department = "";
-		position = "";
-	}
+	public static void main(String[] args) {
+        int month, year;
+		Scanner keyIn = new Scanner(System.in);
 
-	/**
-	 * Zero Constructor accepts no parameters and sets the instance values "", 0
-	*/
-	public Employee () {
-		name = "";
-		idNumber = 0;
-		department = "";
-		position = "";
-	}
+		// communicate
+		System.out.println("\nType in the number for the month and year,\n"
+				+ "pressing enter for each.\n");
 
-	/**
-	 * getName()
-	 * @return String object with the employee's name for the current instance
-	*/
-	public String getName () {
-		return name;
-	}
+		// input + validation
+		do {
+			System.out.print("Month: ");
+			month = keyIn.nextInt();
+		} while (month < 1 || month > 12);
 
-	/**
-	 * setName() sets the instance's name field
-	 * @param n a String object representing the employee's name
-	*/
-	public void setName (String n) {
-		name = n;
-	}
+		do {
+			System.out.print("Year: ");
+			year = keyIn.nextInt();
+		} while (year < 0);
 
-	/**
-	 * getIdNumber()
-	 * @return Int with the employee's idNumber for the current instance
-	*/
-	public String getIdNumber () {
-		return idNumber;
-	}
-
-	/**
-	 * setIdNumber() sets the instance's idNumber field
-	 * @param id an integer representing the employee's IdNumber
-	*/
-	public void setIdNumber (int id) {
-		idNumber = id;
-	}
-
-	/**
-	 * getDepartment()
-	 * @return String object with the employee's department for the current instance
-	*/
-	public String getDepartment () {
-		return department;
-	}
-
-	/**
-	 * setDepartment() sets the instance's department field
-	 * @param dep a String object representing the employee's department
-	*/
-	public void setDepartment (String dep) {
-		department = dep;
-	}
-
-	/**
-	 * getPosition()
-	 * @return String object with the employee's position for the current instance
-	*/
-	public String getPosition () {
-		return position;
-	}
-
-	/**
-	 * setPosition() sets the instance's position field
-	 * @param pos a String object representing the employee's position
-	*/
-	public void setPosition (String pos) {
-		position = pos;
+		// output
+		MonthDays date = new MonthDays(month, year);
+		System.out.println(date.getNumberOfDays() + " days\n");
 	}
 }
--- oocpp/code/practice.cpp
+++ oocpp/code/practice.cpp
@@ -1,45 +1,25 @@
 /*
   Carlos Vigil
-  Dr. . Laurasi
+  Dr. T. Laurasi
   OOC++
 */
 #include <iostream>
 using namespace std;
 
-int input_employees();
-int input_absences(int);
-double average_absences(int, int);
+int main()
+{
+	const int NUM_FISH = 20;
+	int fish[NUM_FISH];
 
-int main () {
-	int number_employees;
-	int days_absent;
-	double average_days_absent;
+    for (int i = 0; i < NUM_FISH; i++) {
+        cout << "\nFisherman " << i+1 << endl << "How many fish were caught?\n";
+        cin >> fish[i];
+        cout << "ok, " << fish[i] << " fish were caught\n";
+    }
+	// You must finish this program. It should ask how
+	// many fish were caught by fishermen 1-20, and
+	// store this data in the array fish.
 
-	number_employees = input_employees();
-	days_absent = input_absences(number_employees);
-
-	cin.get();
 	return 0;
 }
 
-int input_employees () {
-	int num_employees;
-
-	cout << "\nEnter the number of employees: ";
-	cin >> num_employees;
-
-	return num_employees;
-}
-
-int input_absences (int employees) {
-	int total_absences;
-	int absences;
-
-	for (; employees > 0; employees--) {
-		cout << "\nEnter how times employee " << employees << " was absent: ";
-		cin >> absences;
-		total_absences += absences;
-	}
-
-	return total_absences;
-}
