f8af2661d70ee1e3c18159689cef641b17149f91
Carlos Vigil <carlos.vigil@icloud.com>
2017-04-30 20:46:44 -0400

    split a major function in QuarterlySales.java, leave a TODO

--- java/code/QuarterlySales.java
+++ java/code/QuarterlySales.java
@@ -13,15 +13,17 @@ public class QuarterlySales {
     final int QUARTERS = 4;
     // arrays for sales storage
     double[][] sales;
-    // scanner for keyboard input
-    Scanner keyb = new Scanner(System.in);
     // variables for calculations
     double companySales, companyQuarter, quarterSales, quarterAverage, divisionSales;
+    // scanner for keyboard input
+    Scanner keyb = new Scanner(System.in);
 
+    // Constructor; creates array set
     public QuarterlySales () {
         sales = new double[DIVISIONS][QUARTERS];
     }
 
+    // MUTATORS
     /**
      * setAllDivisionSales Procedurally asks for and enters the sales data for
      * each of 4 quarters and 6 divisions.
@@ -40,27 +42,45 @@ public class QuarterlySales {
         }
     }
 
+    /**
+     * setQuarterSales sets the sales for a single quarter within a division
+     * @param division Integer of the division selected
+     * @param quarter Integer of the quarter selected
+    */
     public void setQuarterSales (int division, int quarter) {
         System.out.print("Enter the sales: ");
         sales[division][quarter] = keyb.nextDouble();
     }
 
+    // ACCESSORS
+    /**
+     * getAllSales displays all sales data for the year across the company.
+    */
     public void getAllSales () {
-        // throughout the company
-        companySales = 0.0;
-        companyQuarter = 0.0;
-
+        // report labeling
         System.out.println("\nFULL YEAR SALES REPORT");
+
+        getDivisionalSales();
+        getCompanySales();
+    }
+
+    /**
+     * getDivisionalSales displays sales for all divisions.
+    */
+    public void getDivisionalSales () {
+        // report labeling
+        System.out.println("\nDIVISIONAL SALES");
+
         // within each division
         for (int i = 0; i < DIVISIONS; i++) {
             divisionSales = 0.0;
 
+            // division labeling
             System.out.print("\nDivision " + (i+1));
 
             // within each quarter
             for (int j = 0; j < QUARTERS; j++) {
                 divisionSales += sales[i][j];
-                companySales += sales[i][j];
 
                 // output divison quarter sales
                 System.out.print("\nQuarter " + (j+1) + " sales: $" + sales[i][j]);
@@ -75,10 +95,29 @@ public class QuarterlySales {
                     System.out.print("  (- $" + (sales[i][j-1] - sales[i][j]) + ")");
                 }
 
+                // TODO: find top and averages
+
+                // outside of quarter
             }
 
             // output division year sales
             System.out.println("\nYear sales for division: $" + divisionSales);
+        }
+    }
+
+    /**
+     * getCompanySales displays sales totals for the company
+    */
+    public void getCompanySales () {
+        companySales = 0.0;
+        companyQuarter = 0.0;
+        // companySales +=
+
+        // loop through quarters
+        for (int i = 0; i < QUARTERS; i++) {
+            for (int j = 0; j < DIVISIONS; j++) {
+
+            }
         }
 
         // output company sales
